f35166f216fbe16936c82014974747dfd506e538
MIke Lei <mikelei1997@gmail.com>
2023-02-10 16:47:25 -0800

    fix request and response

--- modules/flippBidAdapter.js
+++ modules/flippBidAdapter.js
@@ -1,12 +1,13 @@
-import {isEmpty} from '../src/utils.js';
+import {isEmpty, parseUrl} from '../src/utils.js';
 import { registerBidder } from '../src/adapters/bidderFactory.js';
 import { BANNER } from '../src/mediaTypes.js';
 const NETWORK_ID = 11090;
 const AD_TYPES = [4309, 641];
 const TARGET_NAME = 'inline';
 const BIDDER_CODE = 'flipp';
-const ENDPOINT_URL = 'http://127.0.0.1:7000/prebid_campaigns';
-// const ENDPOINT_URL = 'https://gateflipp-stg.flippback.com/flyer-locator-service-stg/prebid_campaigns';
+const ENDPOINT = 'http://127.0.0.1:7000/prebid_campaigns';
+// const ENDPOINT = 'https://gateflipp-stg.flippback.com/flyer-locator-service-stg/prebid_campaigns';
+// const ENDPOINT = 'https://gateflipp.flippback.com/flyer-locator-service/prebid_campaigns';
 const DEFAULT_TTL = 30;
 const DEFAULT_CURRENCY = 'USD';
 
@@ -22,48 +23,57 @@ export const spec = {
   code: BIDDER_CODE,
   supportedMediaTypes: [BANNER],
   /**
-     * Determines whether or not the given bid request is valid.
-     *
-     * @param {BidRequest} bid The bid params to validate.
-     * @return boolean True if this is a valid bid, and false otherwise.
-     */
+   * Determines whether or not the given bid request is valid.
+   *
+   * @param {BidRequest} bid The bid params to validate.
+   * @return boolean True if this is a valid bid, and false otherwise.
+   */
   isBidRequestValid: function(bid) {
     return !!(bid.params.siteId);
   },
   /**
-     * Make a server request from the list of BidRequests.
-     *
-     * @param {BidRequest[]} validBidRequests[] an array of bids
-     * @param {BidderRequest} bidderRequest master bidRequest object
-     * @return ServerRequest Info describing the request to the server.
-     */
-  buildRequests: (validBidRequests, bidderRequest) => (
-    {
+   * Make a server request from the list of BidRequests.
+   *
+   * @param {BidRequest[]} validBidRequests[] an array of bids
+   * @param {BidderRequest} bidderRequest master bidRequest object
+   * @return ServerRequest Info describing the request to the server.
+   */
+  buildRequests: function(validBidRequests, bidderRequest) {
+    const urlParams = parseUrl(bidderRequest.refererInfo.page).search;
+    const contentCode = urlParams['flipp-content-code'];
+    let userKey;
+    const placements = validBidRequests.map((bid, index) => {
+      userKey = isEmpty(bid.params.userKey) ? generateUUID() : bid.params.userKey;
+      return {
+        divName: TARGET_NAME,
+        networkId: NETWORK_ID,
+        siteId: bid.params.siteId,
+        adTypes: AD_TYPES,
+        count: 1,
+        ...(!isEmpty(bid.params.zoneIds) && {zoneIds: bid.params.zoneIds}),
+        properties: {
+          ...(!isEmpty(contentCode) && {contentCode: contentCode.slice(0, 32)}),
+        },
+        prebid: {
+          requestId: bid.bidId,
+          publisherNameIdentifier: bid.params.publisherNameIdentifier,
+          height: bid.mediaTypes.banner.sizes[index][0],
+          width: bid.mediaTypes.banner.sizes[index][1],
+        }
+      }
+    });
+    return {
       method: 'POST',
-      url: ENDPOINT_URL,
+      url: ENDPOINT,
       data: {
-        placements: validBidRequests.map((bid, index) => (
-          {
-            divName: TARGET_NAME,
-            networkId: NETWORK_ID,
-            siteId: bid.params.siteId,
-            adTypes: AD_TYPES,
-            count: 1,
-            ...(!isEmpty(bid.params.zoneIds) && {zoneIds: bid.params.zoneIds}),
-            properties: {
-              ...(!isEmpty(bid.params.contentCode) && {contentCode: bid.params.contentCode.slice(0, 32)}),
-            },
-            prebid: {
-              requestId: bid.bidId,
-              publisherNameIdentifier: bid.params.publisherNameIdentifier,
-              height: bid.mediaTypes.banner.sizes[index][0],
-              width: bid.mediaTypes.banner.sizes[index][1],
-            },
-            user: {key: isEmpty(bid.params.userKey) ? generateUUID() : bid.params.userKey},
-          })),
+        placements,
         url: bidderRequest.refererInfo.page,
+        user: {
+          key: userKey,
+        },
       },
-    }),
+    }
+  },
   /**
    * Unpack the response from the server into a list of bids.
    *
@@ -74,25 +84,20 @@ export const spec = {
   interpretResponse: function(serverResponse, bidRequest) {
     if (!serverResponse?.body) return [];
     const res = serverResponse.body;
-    const bidResponses = [];
     if (!isEmpty(res) && !isEmpty(res.decisions) && !isEmpty(res.decisions.inline)) {
-      const creative = res.prebid?.creative;
-      const decision = res.decisions.inline[0];
-      const cpm = res.prebid?.cpm;
-      const bidResponse = {
-        requestId: bidRequest.data.prebid.requestId,
-        cpm,
-        width: bidRequest.data.prebid.width,
-        height: bidRequest.data.prebid.height,
+      return res.decisions.inline.map(decision => ({
+        requestId: decision.prebid?.requestId,
+        cpm: decision.prebid?.cpm,
+        width: decision.width,
+        height: decision.height,
         creativeId: decision.adId,
         currency: DEFAULT_CURRENCY,
         netRevenue: true,
         ttl: DEFAULT_TTL,
-        ad: creative,
-      };
-      bidResponses.push(bidResponse);
+        ad: decision.prebid?.creative,
+      }));
     }
-    return bidResponses;
+    return [];
   },
 
   /**
